a19e3e658a2814bf848c403f713a3d4d4f5e7629
VICTOR MANUEL MURILLO CAMAYO <mcvictor@unicauca.edu.co>
2020-11-05 13:12:22 -0500

    clase15

--- App.js
+++ App.js
@@ -2,11 +2,42 @@ import 'react-native-gesture-handler';
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { CoinStack } from './src/components/coins/CoinStack';
-
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { Image } from 'react-native';
+import colors from './src/res/colors'
+const Tabs = createBottomTabNavigator()
 export default App = () => {
   return (
     <NavigationContainer>
-      <CoinStack />
+      <Tabs.Navigator
+        tabBarOptions={{
+          tintColor: '#fefefe',
+          style: {
+            backgroundColor: colors.blackPearl
+          }
+
+        }}
+      >
+        <Tabs.Screen name={'Coins'} component={CoinStack}
+          options={{
+            tabBarIcon: ({ size, color }) => (
+              <Image
+                style={{ tintColor: color, width: size, height: size }}
+                source={require('./src/assets/bank.png')} />
+            )
+          }}
+        />
+        <Tabs.Screen name={'Favorites'} component={CoinStack}
+          options={{
+            tabBarIcon: ({ size, color }) => (
+              <Image
+                style={{ tintColor: color, width: size, height: size }}
+                source={require('./src/assets/bank.png')} />
+            )
+          }}
+        />
+
+      </Tabs.Navigator>
     </NavigationContainer>
   )
 }
